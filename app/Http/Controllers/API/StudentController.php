9f174b6827a51a8fa80fa5d51c568789c47014ea
Sai Chand Reddy Kamana <saichand.kamana@outlook.com>
2023-11-16 18:16:12 +0000

    Develop RESTful APIs for CRUD operations on student records

--- app/Http/Controllers/API/StudentController.php
+++ app/Http/Controllers/API/StudentController.php
@@ -2,10 +2,181 @@
 
 namespace App\Http\Controllers\API;
 
-use App\Http\Controllers\Controller;
+use App\Models\Student;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Http\Controllers\web\StudentController as WebStudentController;
+use App\Http\Controllers\Controller as BaseController;
 
-class StudentController extends Controller
+class StudentController extends BaseController
 {
-    //
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        try {
+            $students = WebStudentController::index($request);
+            if($students->count() > 0) {
+                return response()->json([
+                    'status' => 200,
+                    'students' => $students,
+                ], 200);
+            }else{
+                return response()->json([
+                    'status' => 404,
+                    'message' => 'No students found',
+                ], 404);
+            }
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'Error occurred : '.$e->getMessage()], 500);
+        }
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        try{
+            $validator = Validator::make($request->all(), [
+                'student_id' => 'required|digits:8',
+                'title' => 'in:'. implode(',', array('Dr','Mr', 'Mrs', 'Ms', 'Mx', 'Professor')),
+                'forename_1' => 'string|max:100',
+                'forename_2' => 'string|max:100',
+                'surname' => 'required|string|max:100',
+                'gender' => 'required|in:'. implode(',', array('Male', 'Female', 'Other')),
+                'date_of_birth' => 'required|date',
+                'username' => 'required|string|max:6',
+                'email' => 'required|email|max:255',
+            ]);
+            if($validator->fails()){
+                return response()->json([
+                    'status' => 422,
+                    'errors' => $validator->messages(),
+                ], 422);
+            }else{
+                $studentExists = WebStudentController::checkStudentExists($request);
+                if($studentExists){
+                    $student = WebStudentController::store($request);
+                    return response()->json([
+                        'status' => 200,
+                        'message' => 'Student created successfully with ID : '.$student->student_id
+                    ], 200);
+                }else{
+                    return response()->json([
+                        'status' => 409,
+                        'message' => 'Student already exists with ID or email or username'
+                    ], 409);
+                }
+            }
+        }catch(\Exception $e){
+            return response()->json(['error' => 'Error occurred : '.$e->getMessage()], 500);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show($id)
+    {
+        try{
+            if(strlen($id) == 8){
+                $student = WebStudentController::show($id);
+                if($student != null){
+                    return response()->json([
+                        'status' => 200,
+                        'student' => $student,
+                    ], 200);
+                }else{
+                    return response()->json([
+                        'status' => 404,
+                        'message' => 'Student not found',
+                    ], 404);
+                }
+            }else{
+                return response()->json([
+                    'status' => 404,
+                    'message' => 'The student ID must be 8 digits',
+                ], 404);
+            }
+        }catch(\Exception $e){
+            return response()->json(['error' => 'Error occurred : '.$e->getMessage()], 500);
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        try{
+            if(strlen($id) == 8){
+                $validator = Validator::make($request->all(), [
+                    'title' => 'in:'. implode(',', array('Dr','Mr', 'Mrs', 'Ms', 'Mx', 'Professor')),
+                    'forename_1' => 'string|max:100',
+                    'forename_2' => 'string|max:100',
+                    'surname' => 'required|string|max:100',
+                    'gender' => 'required|in:'. implode(',', array('Male', 'Female', 'Other')),
+                    'date_of_birth' => 'required|date',
+                    'username' => 'required|string|max:6',
+                    'email' => 'required|email|max:255',
+                ]);
+                if($validator->fails()){
+                    return response()->json([
+                        'status' => 422,
+                        'errors' => $validator->messages(),
+                    ], 422);
+                }else{
+                    $student = WebStudentController::update($request, $id);
+                    if($student){
+                        return response()->json([
+                            'status' => 200,
+                            'message' => 'Student updated successfully with ID : '.$id
+                        ], 200);
+                    }
+                    return response()->json([
+                        'status' => 404,
+                        'message' => 'Student not found with ID : '.$id,
+                    ], 404);
+                }
+            }else{
+                return response()->json([
+                    'status' => 404,
+                    'message' => 'The student ID must be 8 digits',
+                ]);
+            }
+        }  catch(\Exception $e){
+            return response()->json(['error' => 'Error occurred : '.$e->getMessage()], 500);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        try{
+            if(strlen($id) == 8){
+                $student = WebStudentController::destroy($id);
+                if($student){
+                    return response()->json([
+                        'status' => 200,
+                        'message' => 'Student deleted successfully with ID : '.$id,
+                    ], 200);
+                }
+                return response()->json([
+                    'status' => 404,
+                    'message' => 'Student not found with ID : '.$id,
+                ], 404);
+            }else{
+                return response()->json([
+                    'status' => 404,
+                    'message' => 'The student ID must be 8 digits',
+                ], 404);
+            }
+        }catch(\Exception $e){
+            return response()->json(['error' => 'Error occurred : '.$e->getMessage()], 500);
+        }
+    }
 }
--- app/Http/Controllers/web/StudentController.php
+++ app/Http/Controllers/web/StudentController.php
@@ -2,10 +2,96 @@
 
 namespace App\Http\Controllers\web;
 
-use App\Http\Controllers\Controller;
+use App\Models\Student;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller as BaseController;
 
-class StudentController extends Controller
+class StudentController extends BaseController
 {
-    //
+    public static function index(Request $request)
+    {
+        try {
+            $students = Student::select('student_id', 'title', 'forename_1', 'forename_2', 'surname', 'gender', 'date_of_birth', 'username', 'email')->where('deleted_at', null) ->get();
+            return $students;
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'Error occurred : '.$e->getMessage()], 500);
+        }
+    }
+
+    public static function store(Request $request){
+        try{
+            $student = new Student();
+            $student->student_id = $request->input('student_id');
+            $student->title = $request->input('title');
+            $student->forename_1 = $request->input('forename_1');
+            $student->forename_2 = $request->input('forename_2');
+            $student->surname = $request->input('surname');
+            $student->gender = $request->input('gender');
+            $student->date_of_birth = $request->input('date_of_birth');
+            $student->username = $request->input('username');
+            $student->email = $request->input('email');
+            $student->save();
+            return $student;
+        }catch(\Exception $e){
+            return response()->json(['error' => 'Error occurred : '.$e->getMessage()], 500);
+        }
+    }
+
+    public static function checkStudentExists(Request $request){
+        try{
+            $student_id = Student::where('student_id',$request->input('student_id'))->first();
+            $email = Student::where('email', $request->input('email'))->first();
+            $username = Student::where('username', $request->input('username'))->first();
+            return $student_id == null && $email == null && $username == null;
+        }catch(\Exception $e){
+            return response()->json(['error' => 'Error occurred : '.$e->getMessage()], 500);
+        }
+    }
+
+    public static function show(string $id){
+        try{
+            $student = Student::find($id);
+            if($student != null && $student['deleted_at'] == null ){
+                $selectedFields = $student ->only(['title', 'forename_1', 'forename_2', 'surname', 'gender', 'date_of_birth', 'username', 'email']);
+                return $selectedFields;
+            }
+            return null;
+        }catch(\Exception $e){
+            return response()->json(['error' => 'Error occurred : '.$e->getMessage()], 500);
+        }
+    }
+
+    public static function destroy(string $id){
+        try{
+            $student = Student::find($id);
+            if($student != null){
+                $student->delete();
+                return true;
+            }
+            return false;
+        }catch(\Exception $e){
+            return response()->json(['error' => 'Error occurred : '.$e->getMessage()], 500);
+        }
+    }
+
+    public static function update(Request $request, string $id){
+        try{
+            $student = Student::find($id);
+            if($student != null){
+                $student->title = $request->input('title');
+                $student->forename_1 = $request->input('forename_1');
+                $student->forename_2 = $request->input('forename_2');
+                $student->surname = $request->input('surname');
+                $student->gender = $request->input('gender');
+                $student->date_of_birth = $request->input('date_of_birth');
+                $student->username = $request->input('username');
+                $student->email = $request->input('email');
+                $student->save();
+                return $student;
+            }
+            return null;
+        }catch(\Exception $e){
+            return response()->json(['error' => 'Error occurred : '.$e->getMessage()], 500);
+        }
+    }
 }
